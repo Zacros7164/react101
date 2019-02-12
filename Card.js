329db632ff6e977715c4501971771c3dc1f9a118
Zac Crosby <Zacros7164@gmail.com>
2019-02-12 12:02:02 -0500

    examples working with props

--- Card.js
+++ Card.js
@@ -1,12 +1,13 @@
 // EVERY REACT COMPONENT MUST RETURN A SINGLE DOM ELEMENT!!!
 
-function Card(){
+function Card(props){
+    console.log(props)
     const name = "Robert Bunch";
-    const title = "React From the Beginning";
+    // const title = "React From the Beginning";
+    const title = props.title;
     const saleOn = false;
 
     return (
-        <div className="row">
             <div className="col s2">
                 <div className="card hoverable small">
                     <div className="card-image">
@@ -21,6 +22,5 @@ function Card(){
                     </div>
                 </div>
             </div>    
-        </div>
     )
 };
