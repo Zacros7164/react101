5d2963e9db032b96005bae05f7f7c1292f9ebf84
Zac Crosby <Zacros7164@gmail.com>
2019-02-12 14:18:02 -0500

    passing data around to react components

--- Card.js
+++ Card.js
@@ -1,21 +1,21 @@
 // EVERY REACT COMPONENT MUST RETURN A SINGLE DOM ELEMENT!!!
 
 function Card(props){
-    console.log(props)
-    const name = "Robert Bunch";
-    // const title = "React From the Beginning";
-    const title = props.title;
+    // console.log(props)
+    // const name = "Robert Bunch";
+    // // const title = "React From the Beginning";
+    // const title = props.title;
     const saleOn = false;
 
     return (
-            <div className="col s2">
+            <div className="col s3">
                 <div className="card hoverable small">
                     <div className="card-image">
-                        <img src="http://lorempixel.com/400/400/nature/" />
+                        <img src={props.image} />
                     </div>
                     <div className="card-content">
-                        <p>{title}</p>
-                        <p>{name}</p>
+                        <p>{props.course}</p>
+                        <p>{props.instructor}</p>
                     </div>
                     <div className="card-action">
                         <a href="#">${saleOn ? 9.99 : 59.99}</a>
